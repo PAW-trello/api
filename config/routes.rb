bda14a0078ed512486c36987268345fa588af61f
msniec <michal.sniec@gmail.com>
2019-11-06 21:49:43 +0100

    Routes hotfix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   end
 
   resources :boards do
+    resources :lists do
 
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
